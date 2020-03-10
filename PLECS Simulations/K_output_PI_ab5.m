61d207fb238947c1ba8fb7bf1bcb80639db9a181
Tuofei Chen <tuofei.chen@gmail.com>
2020-03-10 12:17:30 -0700

    kabc testing with 6 state (Vn deterministic)

--- PLECS Simulations/K_output_PI_ab5.m
+++ PLECS Simulations/K_output_PI_ab5.m
@@ -9,9 +9,9 @@
 % |          |
 % C2---S3----C6
 
-
-Vn = (D1V1+D2V3+D3V1+D4V2+D5V2+D6V3)/3;
-
+% 
+% Vn = (D1V1+D2V3+D3V1+D4V2+D5V2+D6V3)/3;
+% 
 
 
 clear
@@ -19,9 +19,9 @@ close all
 load IV_curve.mat
 % close all; 
 %% Setup the linear system
-Nstate = 5; % Number of states
+Nstate = 6; % Number of states
 Nin    = 6;    % Number of inputs 
-Nout   = 5;   % Number of outputs (last output is not used)
+Nout   = 6;   % Number of outputs (last output is not used)
 
 Nstack = 1; 
 Nskip  = 3; 
@@ -52,10 +52,9 @@ Vin_max = 0.05*100;
 Ig_max  = 0.05*10;
 u_max   = 1; 
 
-Qdiag  = [5e5*ones(1,3),5e6*ones(1,2),1/(Vin_max^2)*ones(1,3),1/(Ig_max^2)*ones(1,2)];
+Qdiag  = [5e5*ones(1,3),5e6*ones(1,2),1/(Vin_max^2)*ones(1,3),1/(Ig_max^2)*ones(1,3)];
 % Qpdiag = [1/(Vin_max^2)*ones(1,3),1/(Ig_max^2)*ones(1,3)];
 % Qp     = diag (Qpdiag); 
-
 
 Q = diag(Qdiag); 
 R = sqrt(1/Nin)*1*eye(Nin); 
@@ -68,16 +67,29 @@ Dnom        = D;
 Ig          = [Ig1(ii),Ig2(ii),Ig3(ii)]; 
 
 
-Aiv = [D(1)-D(3),D(1); -D(4) -D(5); D(2) D(2)-D(6)]/Cin;
-Avi = [D(1) 0 D(2); 0 D(5) D(6);]/Lout;
-A = [zeros(3,3), Aiv; Avi zeros(2,2)]; 
+Aiv = [-D(1),-D(3),0;0 -D(4) -D(5); -D(2) 0 -D(6)]/Cin;
+Avi = [D(1) 0 D(2); D(3) D(4) 0;0 D(5) D(6)]/Lout;
+Avn = [-(D(1)+D(3))/3,-(D(4)+D(5))/3,-(D(2)+D(6))/3;...
+       -(D(1)+D(3))/3,-(D(4)+D(5))/3,-(D(2)+D(6))/3;...
+       -(D(1)+D(3))/3,-(D(4)+D(5))/3,-(D(2)+D(6))/3;]/Lout;
+Avi = Avi+ Avn;    
+
+A = [zeros(3,3), Aiv; Avi zeros(3,3)]; 
 
 Bv = [-Ig(1) 0 -Ig(2) 0 0 0; 
      0 0 0 -Ig(2) -Ig(3) 0; 
-     0 -Ig(1) 0 0 0 -Ig(3)]/Cin; %this is fine nominally
+     0 -Ig(1) 0 0 0 -Ig(3)]/Cin; 
 
-Bi = [0 0 Vpv(1) Vpv(3)  0  0; 
-       0  0  0  0  Vpv(1)   Vpv(2);]/Lout;   
+Bi = [Vpv(1) Vpv(3) 0   0   0  0; 
+       0  0  Vpv(1) Vpv(2)  0  0;
+       0  0  0   0  Vpv(2) Vpv(3)]/Lout;
+
+Bin = [-Vpv(1),-Vpv(3),-Vpv(1),-Vpv(2),-Vpv(2),-Vpv(3);
+       -Vpv(1),-Vpv(3),-Vpv(1),-Vpv(2),-Vpv(2),-Vpv(3);
+       -Vpv(1),-Vpv(3),-Vpv(1),-Vpv(2),-Vpv(2),-Vpv(3);]/3/Lout;
+
+Bi = Bi + Bin; 
+
 B = [Bv; Bi];
 
 C = [eye(Nout), zeros(Nout,Nstate-Nout)];
@@ -99,7 +111,22 @@ Ai = [zeros(Nout+Nstate,Nout),[-C;A]];
 Bi = [-D;B];
 Ci = [zeros(Nout,Nout),C];
 Di = D;
-% 
+
+id_vi = 1; 
+id_ii = 2; 
+id_vp = 3; 
+id_ip = 4; 
+
+Kabc = [K1_log(ii,id_vi),0,0,  K1_log(ii,id_ii),0,0,  K1_log(ii,id_vp),0,0,  K1_log(ii,id_ip),0,0;
+        0,0,K2_log(ii,id_vi),  K2_log(ii,id_ii),0,0,  0,0,K2_log(ii,id_vp),  K2_log(ii,id_ip),0,0;
+        K3_log(ii,id_vi),0,0,  0,K3_log(ii,id_ii),0,  K3_log(ii,id_vp),0,0,  0,K3_log(ii,id_ip),0;
+        0,K4_log(ii,id_vi),0,  0,K4_log(ii,id_ii),0,  0,K4_log(ii,id_vp),0,  0,K4_log(ii,id_ip),0;
+        0,K5_log(ii,id_vi),0,  0,0,K5_log(ii,id_ii),  0,K5_log(ii,id_vp),0,  0,0,K5_log(ii,id_ip);
+        0,0,K6_log(ii,id_vi),  0,0,K6_log(ii,id_ii),  0,0,K6_log(ii,id_vp),  0,0,K6_log(ii,id_ip);
+        ];
+    
+[V,lambda]  = eig(Ai-Bi*Kabc); 
+P = diag(lambda); 
 % Q(Nout+4,Nout+4) = 1/((Ig(1))^2+1);
 % Q(Nout+5,Nout+5) = 1/((Ig(2))^2+1);
 % Q(Nout+6,Nout+6) = 1/((Ig(3))^2+1);
@@ -108,41 +135,42 @@ Di = D;
 % Qp(6,6) = 1/((Ig(3))^2+0.1);
 
 % dD_max = abs(0.5- abs(abs(Dnom)-0.5)); 
-dD_max = 0.1*ones(1,length(Dnom)); 
-Rdiag = 1./(dD_max.^2);
-R = diag(Rdiag); 
-
-try
-    [K,S,P] = lqr(Ai,Bi,Q,R);
-
-%     switch Nskip
-%         case 1       
-%              K = [K(:,1:3),zeros(6,1),K(:,4:end)];        
-%         case 2
-%              K = [K(:,1:4),zeros(6,1),K(:,5:end)];
-%         case 3
-%              K = [K(:,1:5),zeros(6,1),K(:,6:end)]; 
+% dD_max = 0.1*ones(1,length(Dnom)); 
+% Rdiag = 1./(dD_max.^2);
+% R = diag(Rdiag); 
+% 
+% try
+%     [K,S,P] = lqr(Ai,Bi,Q,R);
+% 
+% %     switch Nskip
+% %         case 1       
+% %              K = [K(:,1:3),zeros(6,1),K(:,4:end)];        
+% %         case 2
+% %              K = [K(:,1:4),zeros(6,1),K(:,5:end)];
+% %         case 3
+% %              K = [K(:,1:5),zeros(6,1),K(:,6:end)]; 
+% %     end
+% %     
+% catch
+% %     K = lqr(Ai+randn,Bi,Q,R); 
+%     disp('Warning, LQR Failed!') ;
+%     if (ii==1) 
+%         xnom_log(ii,:) = [Vpv,Ig(1),Ig(2),Ig(3)];
+%         continue; 
+%     end 
+%     K = reshape(K_log(ii-1,:,:),6,11); 
+% end
+% 
+% 
+% if sum(sum(K))~=0
+%     if sum(real(P)>0)
+%         disp('Warning, unstable'); 
 %     end
-%     
-catch
-%     K = lqr(Ai+randn,Bi,Q,R); 
-    disp('Warning, LQR Failed!') ;
-    if (ii==1) 
-        xnom_log(ii,:) = [Vpv,Ig(1),Ig(2),Ig(3)];
-        continue; 
-    end 
-    K = reshape(K_log(ii-1,:,:),6,10); 
-end
-
-
-if sum(sum(K))~=0
-    if sum(real(P)>0)
-        disp('Warning, unstable'); 
-    end
-end
-pole_log(ii)  = max(real(P)); 
-K_log(ii,:,:)  = K; 
+% end
+pole_log(:,ii)  = real(P); % max(real(P)); 
+% K_log(ii,:,:)  = K; 
 xnom_log(ii,:) = [Vpv,Ig(1),Ig(2),Ig(3)];
+
 end
 
 
